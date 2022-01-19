8960c745b4c593ace799b5904dbcb3665ed2e43f
liumaowu <915644859@qq.com>
2022-01-19 15:22:05 +0800

    aaa

--- hecheng/assets/Script/homeBtn.js
+++
@@ -1,9 +0,0 @@
-cc.Class({
-	extends: cc.Component,
-
-	properties: {},
-
-	gohome: function () {
-		window.location.href = "/index.html";
-	}
-});
---
+++ hecheng/src/settings.dd42d.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["wall","ball"],collisionMatrix:[[false,true],[true,true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"",jsList:[],bundleVers:{resources:"6c26f",internal:"45e63",main:"d4c10"}};
